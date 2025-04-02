0cfb795a7570e823917d237c28ac289c0c4f2446
Abhishek Prajapati <bhishekprajapati@gmail.com>
2025-04-03 00:53:21 +0530

    refactor

--- components/pyusd.tsx
+++ components/pyusd.tsx
@@ -3,23 +3,20 @@ import ethereum from "@/lib/ethereum";
 import { formatGwei } from "viem";
 import { FMono } from "./text";
 
-const round = (n: number = 0) =>
-  Math.trunc(n * Math.pow(10, 2)) / Math.pow(10, 2);
-
 export const Quote = async () => {
   const result = await pyusd.getQuote();
   if (!result.success) return "";
 
   const token = result.data.PYUSD[0];
-  const price = token.quote.USD.price;
+  const price = token.quote.USD.price.toFixed(2);
   const change = token.quote.USD.percent_change_1h;
 
   return (
     <span>
-      <FMono>{round(price)}</FMono> USD{" "}
+      <FMono>{price}</FMono> USD{" "}
       <span
         className={change > 0 ? "text-success" : "text-danger"}
-      >{`${change > 0 ? "+" : ""}${round(change)}%`}</span>
+      >{`${change > 0 ? "+" : ""}${change.toFixed(2)}%`}</span>
     </span>
   );
 };
