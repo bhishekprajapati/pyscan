c2f7d1f4ebb4f0306e8dda16b73b6226e1f5eedf
Abhishek Prajapati <bhishekprajapati@gmail.com>
2025-04-11 15:39:47 +0530

    fix: table styles

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -5,11 +5,10 @@ import type { TableProps } from "@heroui/react";
 export const col = (name: string, uid: string) => ({ name, uid });
 
 export const tableClassName: TableProps["className"] =
-  "overflow-x-auto bg-default";
+  "overflow-x-auto bg-grey-700";
 
 export const tableClassNames: TableProps["classNames"] = {
-  th: "bg-transparent border-b border-b-divider text-sm",
-  tbody:
-    "[&>tr:nth-child(2n+1)]:bg-primary [&>tr:nth-child(2n+1)]:bg-opacity-[0.01]",
-  thead: "py-8",
+  th: "bg-primary-100 bg-opacity-[0.04] rounded-none border-b border-b-divider text-sm",
+  tbody: "[&>tr:nth-child(2n+1)]:bg-default",
+  thead: "py-8 [&_*]:!rounded-none",
 };
